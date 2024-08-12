9f25068c6c5f47ed13e213f131767d06cd468ced
Arghya Ghosh <argghosh@amd.com>
2024-08-12 14:18:46 +0530

    add custom changes

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -7,7 +7,7 @@
 ############                                                                                                         ############
 ############                DO NOT MODIFY THIS FILE. THIS FILE IS HASHED AND UPDATED AUTOMATICALLY.                  ############
 ############                    ANY CHANGES MADE TO THIS FILE WILL BE OVERWRITTEN BY COMMITS TO                      ############
-############                       https://github.com/ChrisTitusTech/powershell-profile.git.                         ############
+############                       https://github.com/arghya-ghosh-02/powershell-profile.git.                         ############
 ############                                                                                                         ############
 #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!#
 ############                                                                                                         ############
@@ -43,7 +43,7 @@ function Update-Profile {
     }
 
     try {
-        $url = "https://raw.githubusercontent.com/ChrisTitusTech/powershell-profile/main/Microsoft.PowerShell_profile.ps1"
+        $url = "https://raw.githubusercontent.com/arghya-ghosh-02/powershell-profile/main/Microsoft.PowerShell_profile.ps1"
         $oldhash = Get-FileHash $PROFILE
         Invoke-RestMethod $url -OutFile "$env:temp/Microsoft.PowerShell_profile.ps1"
         $newhash = Get-FileHash "$env:temp/Microsoft.PowerShell_profile.ps1"
@@ -263,6 +263,13 @@ function gc { param($m) git commit -m "$m" }
 
 function gp { git push }
 
+function gpr($branch) {
+    $gitdir = & git rev-parse --git-dir
+    & scp -P 29418 argghosh@gerrit-git.amd.com:hooks/commit-msg "$gitdir/hooks/"
+    & git commit --amend --no-edit
+    & git push origin HEAD:refs/for/$branch
+ }
+
 function g { __zoxide_z github }
 
 function gcl { git clone "$args" }
@@ -420,6 +427,8 @@ ga - Shortcut for 'git add .'.
 gc <message> - Shortcut for 'git commit -m'.
 
 gp - Shortcut for 'git push'.
+
+gpr - Raise PR with review
 
 g - Changes to the GitHub directory.
 
